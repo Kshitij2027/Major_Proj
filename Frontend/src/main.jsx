ad1ba1cdffc9141995d79d3ae22bf08e689a7740
Kshitij2027 <kshitijjain87199@gmail.com>
2024-04-29 16:29:52 +0530

    final commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,21 +1,31 @@
-import React, { createContext, useState } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-export const Context = createContext({IsAuthorized: false}); 
+export const Context = createContext({
+  isAuthorized: false,
+});
 
-const AppWrapper = ()=>{
-  const [IsAuthorized, setIsAuthorized] = useState(false);
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
   const [user, setUser] = useState({});
 
   return (
-    <Context.Provider value={{IsAuthorized, setIsAuthorized, user, setUser}}>
-    <App />
+    <Context.Provider
+      value={{
+        isAuthorized,
+        setIsAuthorized,
+        user,
+        setUser,
+      }}
+    >
+      <App />
     </Context.Provider>
-  )
-}
-ReactDOM.createRoot(document.getElementById('root')).render(
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppWrapper />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
